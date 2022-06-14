b13fae8822d3decae5f4b357a696f89dc4cfa2a0
Gentrit Uka <gentrit-uka@live.com>
2022-06-14 16:53:58 +0200

    updated

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
-let userAge = Number(prompt("How old are you?"));
-let travelDistance = Number(prompt("How far are you travelling (in km)?"));
+// let userAge = Number(prompt("How old are you?"));
+let userAge = prompt("How old are you? (enter a number)");
+while (!/^[0-9]+$/.test(userAge)) {
+    alert("Mate are you kidding me? I said a number!!!");
+    userAge = prompt("Enter a number: ");
+}
+let travelDistance = prompt("How far are you travelling (in km)?");
+while (!/^[0-9]+$/.test(travelDistance)) {
+    alert("Don't piss me off lad, big man ting ye... How many times should I say a number is needed here!!!");
+    travelDistance = prompt("Enter a number lad... the kilometers as a numbeeer, jises:");
+}
+
+
 let thePrice = travelDistance * 0.21;
 
 if (userAge < 18){
