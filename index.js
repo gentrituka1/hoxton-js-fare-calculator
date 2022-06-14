bb1a9d54b7d4ef646440f98192f6e94eed176884
Gentrit Uka <gentrit-uka@live.com>
2022-06-14 16:32:47 +0200

    updated

--- index.js
+++ index.js
@@ -4,13 +4,14 @@ let thePrice = travelDistance * 0.21;
 
 if (userAge < 18){
     let thePriceToPay = thePrice - (thePrice * (20 / 100));
-    let theRealPriceToPay = thePriceToPay.toFixed(2);
-    console.log("Oi m8, you need to drop at least: " + theRealPriceToPay + "£");
+    let theRealPriceToPay = thePriceToPay.toFixed(1);
+    console.log("Oi m8, you need to drop at least: " + "£" + theRealPriceToPay);
 } 
 else if (userAge > 65) {
     let thePriceToPay = thePrice - (thePrice * (40 / 100));
-    let theRealPriceToPay = thePriceToPay.toFixed(2);
-    console.log("Oi grandpa, how u doin, drop at least: " + theRealPriceToPay + "£");
+    let theRealPriceToPay = thePriceToPay.toFixed(1);
+    console.log("Oi grandpa, how u doin, drop at least: " + "£" + theRealPriceToPay);
 } else {
-    console.log("Oi there lad, no discount for you bruv, so you have to pay: " + thePrice);
+    let theRealPriceToPay = thePrice.toFixed(1);
+    console.log("Oi there lad, no discount for you bruv, so you have to pay: " + "£" + theRealPriceToPay);
 }
